69fbb837f3cccdcd28ab00193160d09ce83b15ac
Alessio Felicioni <afelicioni@users.noreply.github.com>
2016-05-08 13:32:58 +0200

    Update product name recognization function

--- wp-content/mu-plugins/wordpress-seo-less-something.php
+++ wp-content/mu-plugins/wordpress-seo-less-something.php
@@ -3,28 +3,46 @@
 Plugin Name: WordPress SEO, less something
 Description: Strips some information strings from original plugin output
 Author: Alessio Felicioni
-Version: 1.0
+Version: 1.1
 Author URI: https://github.com/afelicioni/wordpress-seo-less-something
 */
 
 class WPSEOLS
 {
-    static function cb_ob_start()
+    public static function cb_ob_start()
     {
         ob_start(array('WPSEOLS', 'chomp'));
     }
-    static function cb_end_flush()
+    public static function cb_end_flush()
     {
         ob_end_flush();
     }
-    static function chomp($sCode)
+    public static function chomp($sCode)
     {
         if (defined('WPSEO_PATH'))
         {
-            $sCode = str_replace('<!-- This site is optimized with the ' . ( file_exists(WPSEO_PATH . 'premium/') ? 'Yoast WordPress SEO Premium plugin' : 'Yoast WordPress SEO plugin' ) . ' v' . WPSEO_VERSION . ' - https://yoast.com/wordpress/plugins/seo/ -->', '', $sCode);
-            $sCode = str_replace('<!-- / Yoast WordPress SEO plugin. -->', '', $sCode);
+            $sCode = str_replace('<!-- This site is optimized with the ' . self::YoastProductName(true) . ' - https://yoast.com/wordpress/plugins/seo/ -->', '', $sCode);
+            $sCode = str_replace('<!-- / ' . self::YoastProductName() . '. -->', '', $sCode);
         }
         return $sCode;
+    }
+    private static function isWPSEOPremium()
+    {
+        if (file_exists(WPSEO_PATH . 'premium/'))
+        {
+            return true;
+        }
+        return false;
+    }
+    private static function YoastProductName($bWithVersionIfAvailable = false)
+    {
+        $oWPSEOF = WPSEO_Frontend::get_instance();
+        $_vv = ' v' . WPSEO_VERSION;
+        $_sYoastProductName = substr(substr($oWPSEOF->debug_marker(false), 37), 0, -47);
+        if (!$bWithVersionIfAvailable && substr($_sYoastProductName, -(strlen($_vv))) == $_vv) {
+            $_sYoastProductName = substr($_sYoastProductName, 0, -(strlen($_vv)));
+        }
+        return $_sYoastProductName;
     }
 }
 add_action('get_header', array('WPSEOLS', 'cb_ob_start'));
